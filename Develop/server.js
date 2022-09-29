d5ef8bc34ce0426c369830aea8fbb3775ff68912
ricky8221 <110708834+ricky8221@users.noreply.github.com>
2022-09-28 18:10:36 -0700

    fixing the port for heroku deploy

--- Develop/server.js
+++ Develop/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const apiRoute = require('./routes/apiRoute')
 
-PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
